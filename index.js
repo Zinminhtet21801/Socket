063c4f059e02301dd81876c8fae67cecadb23efa
Zin Min Htet <zetminhtin@gmail.com>
2021-09-18 09:49:34 +0630

    second commit

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 
+app.use(express.static(__dirname+"/public"))
+
 app.get("/",(req,res)=>{
-    res.send("GOOD")
+    res.sendFile("/index.html")
 })
 
 app.listen(PORT,(e)=>{
