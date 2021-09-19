ccfb56dcd96b6e6f1a865cafc8cbbe7aad31cff5
Zin Min Htet <zetminhtin@gmail.com>
2021-09-19 19:33:27 +0630

    final Commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
+const server = app.listen(PORT,(e)=>{
+    e ? console.log("Something went wrong") : console.log("Server is running");
+})
+
+const socket = require("socket.io")
+const io = socket(server)
 
 app.use(express.static(__dirname+"/public"))
 
@@ -8,6 +14,16 @@ app.get("/",(req,res)=>{
     res.sendFile("/index.html")
 })
 
-app.listen(PORT,(e)=>{
-    e ? console.log("Something went wrong") : console.log("Server is running");
+io.on("connection",(socket)=>{
+    console.log("Socket connected",socket.id);
+    socket.on("chat",(data)=>{
+        socket.broadcast.emit("chat",data)
+    })
+
+    socket.on("typing",(name)=>{
+        console.log(name);
+        socket.broadcast.emit("typing",`<p><em>${name} is typing...</em></p>`)
+    })
 })
+
+
